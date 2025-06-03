b26970ed12c876372517086890b4f408568a34c0
cvishak <vishakh@eteaminc.com>
2025-06-03 23:45:38 +0530

    deleted CRA and vite installed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from "flowbite-react";
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-[30px] text-center text-blue-500'>CRM</h1>
+    </>
+  )
+}
+
+export default App
